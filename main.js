4fb9fedb8e66aed05c35ec71f355fa72000f3743
rickdegraaf1 <rick_degraaf@hotmail.nl>
2021-11-09 13:11:45 +0100

    made hdnyancat

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+function changeImage(){
+	//get window X and Y
+	w = window.innerWidth;
+	h = window.innerHeight;
+
+	// get document
+	image = document.getElementById("NYAN")
+	// change document
+	image.width = w;
+	image.height = h;
+	
+
+}
